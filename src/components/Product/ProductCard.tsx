024e6017f34afa672ae56951974436ce68411a3b
RickyCyber(GiauDang) <giau-dang@ecombest.com>
2024-11-03 11:06:28 +0700

    update

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -1,21 +1,21 @@
-import React from "react";
-import Image from "next/image";
+import React from 'react';
+import Image from 'next/image';
 
-import { useRouter } from "next/navigation";
-import Cookies from "js-cookie";
-import { NumericFormat } from "react-number-format";
-import { motion } from "framer-motion";
-import Link from "next/link";
-import { useWishlistStore } from "@/store/useWishList";
+import { useRouter } from 'next/navigation';
+import Cookies from 'js-cookie';
+import { NumericFormat } from 'react-number-format';
+import { motion } from 'framer-motion';
+import Link from 'next/link';
+import { useWishlistStore } from '@/store/useWishList';
 
 function ProductCard({ item }: any) {
   const { addWishList } = useWishlistStore();
   const router = useRouter();
 
   const handleAddWishList = (id: string) => {
-    const isLoggedIn = Cookies.get("printzy_ac_token");
+    const isLoggedIn = Cookies.get('printzy_ac_token');
     if (!isLoggedIn) {
-      router.push("/login");
+      router.push('/login');
     } else {
       addWishList(item.id);
     }
@@ -30,7 +30,7 @@ function ProductCard({ item }: any) {
           transition={{ delay: 0.2 }}
           className="relative w-full h-64"
         >
-          <Link href={"/" + item.slug}>
+          <Link href={'/' + item.slug}>
             <Image
               alt=""
               fill
@@ -64,24 +64,21 @@ function ProductCard({ item }: any) {
           </div>
           <div className="flex cursor-pointer mr-2">
             <div className="p-4 bg-secondary rounded-full text-[15px] font-semibold text-white w-[50px] h-[50px] flex justify-center items-center">
-              {Math.round(
-                ((item.price - item.discountPrice) / item.price) * 100
-              )}
-              % OFF
+              {item.discountPercent}% OFF
             </div>
           </div>
         </div>
       </div>
-      <Link href={"/" + item.slug} key={item._id} className="mt-0.5">
+      <Link href={'/' + item.slug} key={item._id} className="mt-0.5">
         <p className="text-sm line-clamp-1 text-primary font-semibold">
           {item.name}
         </p>
         <div className="flex gap-2 items-center">
           <NumericFormat
-            value={item.discountPrice}
-            displayType={"text"}
+            value={item.price - (item.price * item.discountPercent) / 100}
+            displayType={'text'}
             thousandSeparator={true}
-            prefix={"$"}
+            prefix={'$'}
             renderText={(value) => (
               <p className="text-base font-bold text-primary-price uppercase ">
                 {value}
@@ -90,9 +87,9 @@ function ProductCard({ item }: any) {
           />
           <NumericFormat
             value={item.price}
-            displayType={"text"}
+            displayType={'text'}
             thousandSeparator={true}
-            prefix={"$"}
+            prefix={'$'}
             renderText={(value) => (
               <p className="text-sm text-darkness line-through">{value}</p>
             )}
